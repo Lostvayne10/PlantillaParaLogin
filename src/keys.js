2423f4b5572dc91d13a185ba1fed3082f67990df
Alejandro Infante <antonio.infante@alumnos.udg.mx>
2019-09-27 23:15:44 -0500

    Update keys.js
    actualizacion db

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'userlinks',
-        password: 'user1234',
-        database: 'database_links'
+        host: 'b8cmf97ryko1kgcyfmcb-mysql.services.clever-cloud.com',
+        user: 'ug5w7evmtfvizsct',
+        password: 'oFL6gE4Vipj1gZopwLrU',
+        database: 'b8cmf97ryko1kgcyfmcb'
     }
 }
